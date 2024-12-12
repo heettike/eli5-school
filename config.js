7e4bf8fee276a5effff56d0b1bee26343b11b3e4
heettike <tike@ripen.in>
2024-12-13 02:02:12 +0530

    Remove duplicated files from root

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-const config = {
-    API_URL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://localhost:3003'
-        : 'https://eli5-api.onrender.com'  // Your Render backend URL
-};
-
-export default config;
